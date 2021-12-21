eb3ffaa3498dd18a887082958dacfd402a6878e5
pridekk <pridekk@gmail.com>
2021-12-21 12:50:03 +0900

    타일 소유자 정보 작업
    타일에 속한 위치의 참여자 정보, 소유자 정보 제공 작업 중

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,14 +5,26 @@ initializeApp();
 
 const db = firestore();
 
+
 exports.getOwner = https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const x = Number(req.query.x);
   const y = Number(req.query.y);
   // Push the new message into Firestore using the Firebase Admin SDK.
+  const zoom = Number(req.query.zoom);
+  const scale = 1 << zoom;
 
-  const doc = await db.collection( "landOwners").doc(`${x},${y}`).get();
 
+  const point = project(x, y);
+
+  const tileCoordinate = new PointType(
+      Math.floor((point.x * scale) / TILE_SIZE),
+      Math.floor((point.y * scale) / TILE_SIZE)
+  );
+
+  console.log(tileCoordinate);
+  const doc = await db.collection( "landOwners")
+      .doc(`${tileCoordinate.x},${tileCoordinate.y}`).get();
   let data = null;
   if (doc.exists) {
     data = doc.data();
@@ -25,3 +37,29 @@ exports.getOwner = https.onRequest(async (req, res) => {
   res.json({result: data});
 });
 
+
+// const getTiles = (x: number, y: number) => console.log(x, y);
+const TILE_SIZE = 256;
+
+const project = (lat: number, lng: number) => {
+  let siny = Math.sin((lat * Math.PI) / 180);
+
+  // Truncating to 0.9999 effectively limits latitude to 89.189. This is
+  // about a third of a tile past the edge of the world tile.
+  siny = Math.min(Math.max(siny, -0.9999), 0.9999);
+
+  // eslint-disable-next-line max-len
+  return new PointType(TILE_SIZE * (0.5 + lng / 360), TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI)));
+};
+
+
+// eslint-disable-next-line require-jsdoc
+class PointType {
+  x: number;
+  y: number ;
+  // eslint-disable-next-line require-jsdoc
+  constructor(x:number, y:number) {
+    this.x = x;
+    this.y = y;
+  }
+}
